39a079e33dac5f4815ff8b234deecf40f811ee8f
JuanManuelGimenez <juanmauel20@gmail.com>
2023-04-02 20:51:22 -0300

    Ejercicio 5

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -8,3 +8,44 @@
 // esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje indicando que la persona es mayor de edad (mayor de 16 ).
 // mostrarDatos: devuelve toda la información del objeto.
 // generaDNI(): genera un número aleatorio de 8 cifras.
+const generarDNI = () => Math.floor(Math.random() * (99999999 - 00000000 + 1) + 00000000);
+
+class Persona{
+  constructor(nombre,edad,dni,sexo,peso,altura,anioNacimiento){
+    this.nombre=nombre;
+    this.edad=edad;
+    this.dni=dni;
+    this.sexo=sexo;
+    this.peso=peso;
+    this.altura=altura; 
+    this.anioNacimiento=anioNacimiento; 
+  }
+ mayorDeEdad(){
+    if(this.edad >= 18){
+      document.write(`Es mayor de edad`);
+    }else{
+      document.write(`No es mayor de edad`);
+    }
+  }
+
+  imprimeDatos(){
+    document.write('<h2>Datos</h2>')
+    document.write(`Nombre: ${this.nombre},<br> `);
+    document.write(`Edad: ${this.edad} años,<br> `);
+    document.write(`Dni: ${this.dni}<br>`);
+    document.write(`Sexo: ${this.sexo}<br>`);
+    document.write(`Peso: ${this.peso}<br>`);
+    document.write(`Altura: ${this.altura}<br>`);
+    document.write(`Año de nacimiento: ${this.anioNacimiento} <br>`);
+  }
+
+}
+
+let juan = new Persona ('Juan Mauel Gimenez',33,generarDNI(),'H','104kg','1.78m',1989);
+juan.mayorDeEdad();
+juan.imprimeDatos();
+
+
+let mony = new Persona ('Monica del Valle Sosa',33,generarDNI(),'M','54kg','1.5m',1989);
+mony.mayorDeEdad();
+mony.imprimeDatos();
--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -7,3 +7,53 @@
 // “El libro xxx con ISBN xxx creado por el autor xxx tiene páginas xxx”
 // Crear al menos 2 objetos libros y utilizar el método mostrarLibro();
 // Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
+const mayorCantiadPaginas = (numerosPaginas1,numeroPaginas2,nombre1,nombre2) => {
+    numerosPaginas1 > numeroPaginas2 ? document.write(`El libro ${nombre1} tiene mas paginas que el libro ${nombre2}`): document.write(`El libro ${nombre2} tiene mas paginas que el libro ${nombre1}`);
+  } 
+  class Libro {
+    constructor(isbn, titulo, autor, numeroPaginas) {
+      this.isbn = isbn;
+      this.titulo = titulo;
+      this.autor = autor;
+      this.numeroPaginas = numeroPaginas;
+    }
+  
+    set actualizarIsbn(nuevoIsbn) {
+      this.isbn = nuevoIsbn;
+    }
+    get obtenerIsbn() {
+      return this.isbn;
+    }
+  
+    set actualizarAutor(nuevoAutor) {
+      this.autor = nuevoAutor;
+    }
+    get obtenerAutor() {
+      return this.autor;
+    }
+  
+    set actualizarTitulo(nuevoTitulo) {
+      this.titulo = nuevoTitulo;
+    }
+    get obtenerTitulo() {
+      return this.titulo;
+    }
+  
+    set actualizarNumeroPaginas(nuevoNumeroPaginas) {
+      this.numeroPaginas = nuevoNumeroPaginas;
+    }
+    get obtenerNumeroPaginas() {
+      return this.numeroPaginas;
+    }
+  
+    mostrarLibro(){
+      document.write(`El libro '${this.titulo}' con ISBN ${this.isbn} creador por el autor ${this.autor}, tiene ${this.numeroPaginas} paginas <br>`);
+    }
+  }
+  
+  let libro1 = new Libro(9788409273683,'Play historia','Marc Rollan',1008);
+  let libro2 = new Libro(9781302900823,'The Amazing Spider-Man','Stan Lee',1088);
+  libro1.mostrarLibro();
+  libro2.mostrarLibro();
+  mayorCantiadPaginas(libro1.numeroPaginas,libro2.numeroPaginas,libro1.titulo,libro2.titulo);
+  
